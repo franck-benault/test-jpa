6c585f5e5bf6d54b892e12a80535376bdbf6df68
franck-benault <franck-benault@orange.fr>
2014-09-17 23:04:04 +0200

    update database dependencies

--- mod01-hibernate/src/main/java/net/franckbenault/jpa/hibernate/impl/StudentManagerImpl.java
+++ mod01-hibernate/src/main/java/net/franckbenault/jpa/hibernate/impl/StudentManagerImpl.java
@@ -2,10 +2,22 @@ package net.franckbenault.jpa.hibernate.impl;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import net.franckbenault.jpa.hibernate.Student;
 import net.franckbenault.jpa.hibernate.StudentManager;
 
 public class StudentManagerImpl implements StudentManager {
+	
+	private EntityManagerFactory emf;
+	private EntityManager em;
+	
+	public StudentManagerImpl(String persistenceUnitName) {
+	    emf = Persistence.createEntityManagerFactory(persistenceUnitName);
+	    em = emf.createEntityManager();		
+	}
 
 	@Override
 	public Student createStudent(Student student) {
