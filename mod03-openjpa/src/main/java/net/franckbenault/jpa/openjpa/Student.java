ea1b30dce8d3341cc0ad636ef0f0e5bb6bfe9fd2
franck-benault <franck-benault@orange.fr>
2014-09-20 14:48:41 +0200

    add openjpa sample

---
+++ mod03-openjpa/src/main/java/net/franckbenault/jpa/openjpa/Student.java
@@ -0,0 +1,49 @@
+package net.franckbenault.jpa.openjpa;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+
+@Entity
+public class Student {
+  @Id
+  private long id = System.currentTimeMillis();
+
+  //the annotation basic is not needed for Hibernate and EclipseLink
+  //@Basic
+  private String name;
+
+  //hibernate could accept @Basic here
+  //@Basic
+  @Temporal(javax.persistence.TemporalType.TIMESTAMP)  
+  private Date dateOfBirth = new Date();
+
+  public Date getDateOfBirth() {
+    return dateOfBirth;
+  }
+
+  public void setDateOfBirth(Date dateOfBirth) {
+    this.dateOfBirth = dateOfBirth;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  
+}
+
