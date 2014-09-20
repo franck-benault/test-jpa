fc0ab97e1dbf3b2afd6e12f956a2639d3aa18056
franck-benault <franck-benault@orange.fr>
2014-09-20 09:45:02 +0200

    add sample with eclipseLink

---
+++ mod02-eclipselink/src/main/java/net/franckbenault/jpa/eclipselink/Student.java
@@ -0,0 +1,46 @@
+package net.franckbenault.jpa.eclipselink;
+
+import java.util.Date;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+  @Id
+  private long id = System.currentTimeMillis();
+
+  @Basic
+  private String name;
+
+  @Basic
+  private Date dateOfBirth = new Date();
+
+  public Date getDateOfBirth() {
+    return dateOfBirth;
+  }
+
+  public void setDateOfBirth(Date dateOfBirth) {
+    this.dateOfBirth = dateOfBirth;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  
+}
+
