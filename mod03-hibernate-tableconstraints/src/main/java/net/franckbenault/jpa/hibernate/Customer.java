4c3e12ce297a63ba3abf8a026e03e766c0ed4196
franck-benault <franck-benault@orange.fr>
2014-09-21 12:13:14 +0200

    add mod03 constraint on tables

---
+++ mod03-hibernate-tableconstraints/src/main/java/net/franckbenault/jpa/hibernate/Customer.java
@@ -0,0 +1,33 @@
+package net.franckbenault.jpa.hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+
+	  @Id
+	  private long id = System.currentTimeMillis();
+
+	  private String firstname;
+	  
+	private String lastname;
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	  
+
+}
